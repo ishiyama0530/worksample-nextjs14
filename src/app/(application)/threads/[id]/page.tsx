c328e53ed955d487c41929d171f8197b357b6af3
Masanobu Ishiyama <ishiyama@MasanobunoMacBook-Pro.local>
2024-09-03 12:52:51 +0900

    wip

--- src/app/(application)/threads/[id]/page.tsx
+++ src/app/(application)/threads/[id]/page.tsx
@@ -3,12 +3,22 @@ import { PrismaClient } from "@prisma/client";
 
 export type ThreadDetailPageProps = { params: { id: string } };
 
+const getThread = async (id: string) => {
+  try {
+    const prisma = new PrismaClient();
+    const thread = await prisma.thread.findFirst();
+
+    return thread;
+  } catch (e) {
+    console.log(e);
+    return null;
+  }
+};
+
 export default async function ThreadDetailPage({
   params: { id },
 }: ThreadDetailPageProps) {
-  const prisma = new PrismaClient();
-  const thread = await prisma.thread.findFirst();
-
+  const thread = await getThread(id);
   return (
     <>
       <ul className="text-center p-8">
