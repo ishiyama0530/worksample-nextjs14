a1080a5de07311c7a20743642a7c67a0e7792554
Masanobu Ishiyama <ishiyama@MasanobunoMacBook-Pro.local>
2024-09-08 11:09:14 +0900

    wip

--- src/components/thread-create-form.tsx
+++ src/components/thread-create-form.tsx
@@ -119,6 +119,9 @@ export function ThreadCreateForm({ className }: ThreadCreateFormProps) {
           作成する！
         </FormButton>
       </form>
+      <div className="flex justify-center py-2">
+        <TermsCheckBox checked={isTermsChecked} onClick={setTermsChecked} />
+      </div>
     </div>
   );
 }
--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -6,7 +6,7 @@ import { cn } from "@/lib/utils";
 import { Loader2 } from "lucide-react";
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 active:scale-[.98]",
+  "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
   {
     variants: {
       variant: {
@@ -47,9 +47,9 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
       className,
       variant,
       size,
-      asChild,
+      asChild = false,
       isLoading,
-      children = false,
+      children,
       ...props
     },
     ref,
